dd067ac67e332684b37840e2b5331a45bd27c90e
Jonathan Reeve <jon.reeve@gmail.com>
2016-09-16 10:33:42 -0400

    fix setup problems

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from distutils.core import setup
+from sys import version_info as ver
 
-if sys.version_info[0] < 3:
-    version = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
+if ver[0] < 3:
+    version = str(ver[0]) + '.' + str(ver[1])
     sys.exit("""
     Error: your Python version is %s, but macro-etym requires at least
     Python 3. Please upgrade your Python installation, or try using pip3
     instead of pip.""" % version)
 
 setup(
-  name = 'chapterize',
-  packages = ['chapterize'], 
-  version = '0.1.0',
-  description = 'A tool for breaking etexts into chapters.',
-  author = 'Jonathan Reeve',
-  author_email = 'jon.reeve@gmail.com',
-  url = 'https://github.com/JonathanReeve/chapterize', 
-  download_url = 'https://github.com/JonathanReeve/chapterize/tarball/0.1.0', 
-  keywords = ['NLP', 'text', 'chapters'],
-  entry_points='''
-  [console_scripts]
-  chapterize = chapterize.chapterize:cli''',
+    name = 'chapterize',
+    packages = ['chapterize'], 
+    version = '0.1.1',
+    description = 'A tool for breaking etexts into chapters.',
+    author = 'Jonathan Reeve',
+    author_email = 'jon.reeve@gmail.com',
+    url = 'https://github.com/JonathanReeve/chapterize', 
+    download_url = 'https://github.com/JonathanReeve/chapterize/tarball/0.1.1', 
+    install_requires = ['Click'],
+    keywords = ['NLP', 'text', 'chapters'],
+    entry_points='''
+    [console_scripts]
+    chapterize = chapterize.chapterize:cli''',
 )
