f30b1e6d575d0fd59ffa341795341336652c5ebf
Jonathan Reeve <jon.reeve@gmail.com>
2016-09-16 10:28:46 -0400

    package for distribution in pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+if sys.version_info[0] < 3:
+    version = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
+    sys.exit("""
+    Error: your Python version is %s, but macro-etym requires at least
+    Python 3. Please upgrade your Python installation, or try using pip3
+    instead of pip.""" % version)
+
+setup(
+  name = 'chapterize',
+  packages = ['chapterize'], 
+  version = '0.1.0',
+  description = 'A tool for breaking etexts into chapters.',
+  author = 'Jonathan Reeve',
+  author_email = 'jon.reeve@gmail.com',
+  url = 'https://github.com/JonathanReeve/chapterize', 
+  download_url = 'https://github.com/JonathanReeve/chapterize/tarball/0.1.0', 
+  keywords = ['NLP', 'text', 'chapters'],
+  entry_points='''
+  [console_scripts]
+  chapterize = chapterize.chapterize:cli''',
+)
