91b57b756cf9a5cfaf5681f3f8436cfb6a2f48cf
Jonathan Reeve <jon.reeve@gmail.com>
2018-08-05 19:06:09 -0700

    increment version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from sys import version_info as ver
 if ver[0] < 3:
     version = str(ver[0]) + '.' + str(ver[1])
     sys.exit("""
-    Error: your Python version is %s, but macro-etym requires at least
+    Error: your Python version is %s, but chapterize requires at least
     Python 3. Please upgrade your Python installation, or try using pip3
     instead of pip.""" % version)
 
 setup(
     name = 'chapterize',
     packages = ['chapterize'], 
-    version = '0.1.3',
+    version = '0.1.5',
     description = 'A tool for breaking etexts into chapters.',
     author = 'Jonathan Reeve',
     author_email = 'jon.reeve@gmail.com',
